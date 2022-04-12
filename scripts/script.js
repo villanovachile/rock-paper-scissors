85696e581e33bb81526a15541b4607cedbdf5cc3
Daniel Rodriguez <daniel.r@a8c.com>
2022-04-11 22:23:33 -0700

    Updated JS to make choices reflect in UI

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+/*
 
 let playerScore = 0;
 let computerScore = 0;
@@ -90,3 +91,110 @@ let roundOutCome;
 
 
 game();
+
+*/
+
+
+let roundIsActive = false;
+let playerChoice = '';
+
+
+let playerSelectionRock = document.querySelector('#playerSelectionRock');
+let playerSelectionPaper = document.querySelector('#playerSelectionPaper');
+let playerSelectionScissors = document.querySelector('#playerSelectionScissors');
+let playerSelectionRockClass = document.querySelector('.playerSelectionRock');
+let playerSelectionPaperClass = document.querySelector('.playerSelectionPaper');
+let playerSelectionScissorsClass = document.querySelector('.playerSelectionScissors');
+let playerOutcomeChoice = document.querySelector('.playerOutcomeChoice');
+
+let playerSelectors = document.getElementsByClassName('playerSelectors')
+
+
+let restartGame = document.querySelector('#restartGame');
+restartGame.addEventListener('click',function(e) {
+    roundIsActive = false;
+    enableSelections();
+    if (playerChoice !=='') {
+    playerOutcomeChoice.classList.remove(`playerOutcomeChoice${playerChoice}`);
+    playerChoice = '';
+    }
+});
+
+
+
+function playerSelection(selection) {
+
+    let playerSelectors = document.querySelectorAll('.playerSelectors')
+
+    let playerSelection= document.querySelector(`#playerSelection${selection}`);
+    playerOutcomeChoice.classList.remove(`playerOutcomeChoiceRock`);
+    playerOutcomeChoice.classList.remove(`playerOutcomeChoicePaper`);
+    playerOutcomeChoice.classList.remove(`playerOutcomeChoiceScissors`);
+    playerOutcomeChoice.classList.add(`playerOutcomeChoice${selection}`);
+    roundIsActive = true;
+    disableSelections();
+    
+}
+
+
+function disableSelections() {
+
+    for (let i = 0; i < playerSelectors.length; i++) {
+        if  (playerSelectors[i].id == 'playerSelectionRock') {
+            playerSelectors[i].classList.remove('playerSelectionRock');
+            playerSelectors[i].classList.add('playerSelectionRockSelected');
+        } else if (playerSelectors[i].id == 'playerSelectionPaper') {
+            playerSelectors[i].classList.remove('playerSelectionPaper');
+            playerSelectors[i].classList.add('playerSelectionPaperSelected');
+        } else if (playerSelectors[i].id == 'playerSelectionScissors') {
+            playerSelectors[i].classList.remove('playerSelectionScissors');
+            playerSelectors[i].classList.add('playerSelectionScissorsSelected');
+
+         }
+    }
+}
+
+function enableSelections() {
+    for (let i = 0; i < playerSelectors.length; i++) {
+        if  (playerSelectors[i].id == 'playerSelectionRock') {
+            playerSelectors[i].classList.add('playerSelectionRock');
+            playerSelectors[i].classList.remove('playerSelectionRockSelected');
+        } else if (playerSelectors[i].id == 'playerSelectionPaper') {
+            playerSelectors[i].classList.add('playerSelectionPaper');
+            playerSelectors[i].classList.remove('playerSelectionPaperSelected');
+        } else if (playerSelectors[i].id == 'playerSelectionScissors') {
+            playerSelectors[i].classList.add('playerSelectionScissors');
+            playerSelectors[i].classList.remove('playerSelectionScissorsSelected');
+
+         }
+    }
+}
+
+playerSelectionRockClass.addEventListener('click', function(e) {
+    if (!roundIsActive) {
+    playerChoice = 'Rock';
+    playerSelection('Rock');
+   
+}
+    
+});
+
+playerSelectionPaperClass.addEventListener('click', function(e) {
+    if (!roundIsActive) {
+        playerChoice = 'Paper';
+        playerSelection('Paper');
+    }
+        
+    });
+
+playerSelectionScissorsClass.addEventListener('click', function(e) {
+    if (!roundIsActive) {
+        playerChoice = 'Scissors';
+        playerSelection('Scissors');
+    }    
+            
+        });
+
+
+
+
