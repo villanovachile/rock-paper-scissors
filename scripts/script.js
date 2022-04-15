cb148ef4f8741300ae1639fe5c6b3a8d9153cfa8
Daniel Rodriguez <daniel.r@a8c.com>
2022-04-14 21:45:30 -0700

    Added functions to declare winner, and game ending conditions

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,3 @@
-/*
-
-
-
-
-
-
-
-function game(){ // Plays 5 rounds, and concludes an overall winner
-let roundOutCome;
-    for (let i = 1; i <= 5; i++) {
-        roundOutcome = playRound(playerPlay(), computerPlay());
-        
-        if (roundOutcome == null) {
-            break;
-        } else {
-            console.log(roundOutcome);
-        }
-    
-
-
-        console.log(`Round ${i} of 5`);
-        console.log(`Your Score: ${playerScore}`);
-        console.log(`Computer Score: ${computerScore}`);
-        console.log(`===================================`);
-     }
-
-     if (computerScore > playerScore && roundOutcome !== null) {
-         console.log(`The computer has won with a score of ${computerScore} to ${playerScore}`);
-     } else if (computerScore < playerScore && roundOutcome !== null) {
-        console.log(`You've won won with a score of ${playerScore} to ${computerScore}`);
-     } else if (computerScore == playerScore && roundOutcome !== null) {
-        console.log(`It's a tie with a score of ${playerScore} to ${computerScore}`); 
-     } else {
-        console.log(`You have canceled the game`)   
-        
-     }
-
-}
-
-
-game();
-
-*/
-
 let playerScore = 0;
 let computerScore = 0;
 let gameIsActive = false
@@ -71,16 +26,6 @@ computerOutcomeScore.textContent = `Computer: ${computerScore}`;
 
 
 let restartGame = document.querySelector('#restartGame');
-restartGame.addEventListener('click',function(e) {
-    roundIsActive = false;
-    enableSelections();
-    if (playerChoice !=='') {
-        computerOutcomeChoice.style.transitionDuration = "0.7s";
-        playerOutcomeChoice.classList.remove(`playerOutcomeChoice${playerChoice}`);
-        playerChoice = '';
-        computerOutcomeChoice.style.transitionDuration = "0s";
-    }
-});
 
 
 function computerPlay (){ // randomly select a computer choice
@@ -95,14 +40,9 @@ function computerPlay (){ // randomly select a computer choice
 }
 
 
-function playerSelection(selection) {
+function playerSelection(selection) { // Display player's choice and disable buttons
 
     let playerSelectors = document.querySelectorAll('.playerSelectors')
-
-   // let playerSelection= document.querySelector(`#playerSelection${selection}`);
-    playerOutcomeChoice.classList.remove(`playerOutcomeChoiceRock`);
-    playerOutcomeChoice.classList.remove(`playerOutcomeChoicePaper`);
-    playerOutcomeChoice.classList.remove(`playerOutcomeChoiceScissors`);
     playerOutcomeChoice.classList.add(`playerOutcomeChoice${selection}`);
     roundIsActive = true;
     disableSelections();
@@ -111,37 +51,29 @@ function playerSelection(selection) {
 
 
 
-
-
-function computerSelection (selection){
-
+function computerSelection (selection){ // cycle through images & display computer selection
+    computerOutcomeChoice.style.transitionDuration = "0s";
     let x = 0;
     
-    
       let intervalID = setInterval(() =>  {
-
         if (computerChoiceDisplay == 'Rock') {
             computerChoiceDisplay = 'Paper';
             computerOutcomeChoice.classList.add(`computerOutcomeChoicePaper`);
             computerOutcomeChoice.classList.remove(`computerOutcomeChoiceRock`);  
         } else if (computerChoiceDisplay == 'Paper') {
             computerChoiceDisplay = 'Scissors';
-            //computerOutcomeChoice.style.backgroundImage = "url('./images/scissors.png')";
             computerOutcomeChoice.classList.add(`computerOutcomeChoiceScissors`);
             computerOutcomeChoice.classList.remove(`computerOutcomeChoicePaper`);
         } else if (computerChoiceDisplay == 'Scissors') {
             computerChoiceDisplay = 'Rock';
             computerOutcomeChoice.classList.add(`computerOutcomeChoiceRock`);
             computerOutcomeChoice.classList.remove(`computerOutcomeChoiceScissors`);
-            //computerOutcomeChoice.style.backgroundImage = "url('./images/rock.png')";
         }
         if (++x === 20) {  
             clearInterval(intervalID) 
             computerOutcomeChoice.classList.remove(`computerOutcomeChoiceScissors`);
             computerOutcomeChoice.classList.remove(`computerOutcomeChoicePaper`);
-            computerOutcomeChoice.classList.remove(`computerOutcomeChoiceRock`);  
-            //computerOutcomeChoice.style.backgroundImage = "url('./images/outcome-placeholder.png')";
-           // computerOutcomeChoice.classList.remove(`computerOutcomeChoice`);   
+            computerOutcomeChoice.classList.remove(`computerOutcomeChoiceRock`);
             computerOutcomeChoice.classList.add(`computerOutcomeChoice${selection}`);  
             
           }  
@@ -155,45 +87,57 @@ function computerSelection (selection){
 function playRound (playerSelection ,computerSelection) { // compares user's choice with computer & declare's a winner
     
 
- /*   while (playerSelection !=='Rock' || playerSelection !=='Paper' || playerSelection !=='Scissors'){
-        if (playerSelection !== null && (playerSelection =='Rock' || playerSelection =='Paper' ||
-                playerSelection =='Scissors')) {
-            break;
-        } else if (playerSelection !== null) {
-            console.log(`"${playerSelection}" is an invalid choice, try again`);
-            playerSelection = playerPlay();
-        } else {
-        playerSelection = null;
-            break;
-        }
-    }   
-*/
-    roundIsActive = true;
-    disableSelections();
-
-
-
     if (playerSelection=='Rock' && computerSelection=='Paper' ||
             playerSelection=='Paper' && computerSelection=='Scissors' ||
             playerSelection=='Scissors' && computerSelection=='Rock') {
         computerScore++;
-        //console.log(`The computer chose ${computerSelection} and you choose ${playerSelection}`);
-        return `You lose this round! ${computerSelection} beats ${playerSelection}`;
+        setTimeout(function(){displayNotice(`You lose this round! ${computerSelection} beats ${playerSelection}`)},3000);
+        setTimeout(endRound, 7500);
+    
     } else if (playerSelection==computerSelection) {
-       // console.log(`The computer chose ${computerSelection} and you choose ${playerSelection}`);
-        return `This round is a tie! You and the computer chose ${playerSelection}`;
-    } else if (playerSelection==null) {
-        return null;
+        setTimeout(function(){displayNotice(`This round is a tie! You and the computer chose ${playerSelection}`)},3000);
+        setTimeout(endRound, 7500);
     } else {
         playerScore++;
-        //console.log(`The computer chose ${computerSelection} and you choose ${playerSelection}`);
-        return `You won this round! ${playerSelection} beats ${computerSelection}!`;
-        
+        setTimeout(function(){displayNotice(`You won this round! ${playerSelection} beats ${computerSelection}!`)},3000);
+        setTimeout(endRound, 7500);
+    }
+
+
+}
+
+function endGame() { // display game outcome message
+    if (playerScore >= 5) {
+        displayEndGameNotice('Congrats, you beat the computer!');
+    } else {
+        displayEndGameNotice('The computer has beaten you, better luck next time!');
+
+    }
+    
+
+}
+
+function endRound() { // update score & run re-enable buttons function
+        playerOutcomeScore.textContent = `You: ${playerScore}`;
+        computerOutcomeScore.textContent = `Computer: ${computerScore}`;
+        restartGame.classList.add('buttons'); 
+        restartGame.classList.remove('buttonsDisabled');
+        restartGame.disabled = false;
+    
+    if (playerScore >= 5 || computerScore >= 5){
+        endGame();
+    } else {
+        enableSelections();
+        roundIsActive = false;
+        if (playerChoice !=='') {
+        playerOutcomeChoice.classList.remove(`playerOutcomeChoice${playerChoice}`);
+        playerChoice = '';
+        }
     }
 }
 
 
-function disableSelections() {
+function disableSelections() { // disable buttons while round is active
 
     for (let i = 0; i < playerSelectors.length; i++) {
         if  (playerSelectors[i].id == 'playerSelectionRock') {
@@ -208,9 +152,14 @@ function disableSelections() {
 
          }
     }
+    restartGame.classList.remove('buttons'); 
+    restartGame.classList.add('buttonsDisabled');
+    restartGame.disabled = true;
+    
+
 }
 
-function enableSelections() {
+function enableSelections() { // re-enable buttons
     for (let i = 0; i < playerSelectors.length; i++) {
         if  (playerSelectors[i].id == 'playerSelectionRock') {
             playerSelectors[i].classList.add('playerSelectionRock');
@@ -221,44 +170,82 @@ function enableSelections() {
         } else if (playerSelectors[i].id == 'playerSelectionScissors') {
             playerSelectors[i].classList.add('playerSelectionScissors');
             playerSelectors[i].classList.remove('playerSelectionScissorsSelected');
-
          }
     }
-    computerOutcomeChoice.classList.remove(`computerOutcomeChoice${computerChoice}`);
+    restartGame.classList.add('buttons'); 
+    restartGame.classList.remove('buttonsDisabled');
+    restartGame.disabled = false;
+    computerOutcomeChoice.style.transitionDuration = "0.7s";
+    computerOutcomeChoice.classList.remove(`computerOutcomeChoiceRock`);
+    computerOutcomeChoice.classList.remove(`computerOutcomeChoicePaper`);
+    computerOutcomeChoice.classList.remove(`computerOutcomeChoiceScissors`);
 }
 
-playerSelectionRockClass.addEventListener('click', function() {
+
+function displayNotice(message) { // display round results
+    var x = document.getElementById("notice");
+    x.className = "show";
+    let msg = document.getElementById("msg");
+    msg.textContent = message;
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 4500);
+}
+
+  function displayEndGameNotice(message) { // display non-fading end game message
+    let x = document.getElementById("notice");
+    x.className = "end";
+    let msg = document.getElementById("msg");
+    msg.textContent = message;
+    //setTimeout(function(){ x.className = x.className.replace("showEnd", ""); }, 4500);
+  }
+
+
+playerSelectionRockClass.addEventListener('click', function() { // runs user choice and begins round
+
     if (!roundIsActive) {
     playerChoice = 'Rock';
     computerChoice = computerPlay();
-    console.log(computerChoice);
     playerSelection('Rock');
     computerSelection(computerChoice);
+    playRound(playerChoice,computerChoice);
    
 }
     
 });
 
-playerSelectionPaperClass.addEventListener('click', function() {
+playerSelectionPaperClass.addEventListener('click', function() {  // runs user choice and begins round
     if (!roundIsActive) {
         playerChoice = 'Paper';
         computerChoice = computerPlay();
         playerSelection('Paper');
         computerSelection(computerChoice);
+        playRound(playerChoice,computerChoice);
     }
+
         
     });
 
-playerSelectionScissorsClass.addEventListener('click', function() {
+playerSelectionScissorsClass.addEventListener('click', function() {  // runs user choice and begins round
     if (!roundIsActive) {
         playerChoice = 'Scissors';
         computerChoice = computerPlay();
         playerSelection('Scissors');
         computerSelection(computerChoice);
+        playRound(playerChoice,computerChoice);
     }    
-            
-        });
+});
 
-
-
-
+restartGame.addEventListener('click',function(e) { // Restart Game
+    roundIsActive = false;
+    computerScore = 0;
+    playerScore = 0;
+    playerOutcomeScore.textContent = `You: ${playerScore}`;
+    computerOutcomeScore.textContent = `Computer: ${computerScore}`;
+    x = document.getElementById("notice");
+    x.className = x.className.replace("show", "");
+    x.className = x.className.replace("end", "");
+       enableSelections();
+     if (playerChoice !=='') {
+        playerOutcomeChoice.classList.remove(`playerOutcomeChoice${playerChoice}`);
+        playerChoice = '';
+     }
+ });
